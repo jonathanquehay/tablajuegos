6aa03a15fe9dc201a90b78350b6dab9c549b6ebb
jonathanquehay <jjmore83@gmail.com>
2023-06-09 14:42:57 -0600

    para cambiar nombres

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const filasJugadores = document.querySelectorAll('tbody tr');
 
 // Crear un objeto para almacenar los contadores y porcentajes de cada jugador
 const estadisticasJugadores = {};
+// Iterar sobre cada fila de jugadores
 
 // Función para calcular el porcentaje de rendimiento
 function calcularPorcentaje(aciertos, total) {
@@ -53,18 +54,40 @@ function actualizarResultados() {
 filasJugadores.forEach(function (fila) {
   // Obtener el nombre del jugador de la primera celda de la fila
   const nombreJugador = fila.querySelector('td:first-child').textContent;
+  const celdaNombre = fila.querySelector('.nombre-jugador');
 
-  // Verificar si el jugador ya existe en el objeto de estadísticas
-  if (!estadisticasJugadores.hasOwnProperty(nombreJugador)) {
-    // Si no existe, inicializar las estadísticas para ese jugador
-    estadisticasJugadores[nombreJugador] = {
-      saquePositivo: 0,
-      saqueNegativo: 0,
-      recepcionPositivo: 0,
-      recepcionNegativo: 0,
-      ataquePositivo: 0,
-      ataqueNegativo: 0
-    };
+  if (celdaNombre) {
+    // Obtener el nombre del jugador de la celda
+    const nombreJugador = celdaNombre.textContent;
+
+    // Verificar si el jugador ya existe en el objeto de estadísticas
+    if (!estadisticasJugadores.hasOwnProperty(nombreJugador)) {
+      // Si no existe, inicializar las estadísticas para ese jugador
+      estadisticasJugadores[nombreJugador] = {
+        saquePositivo: 0,
+        saqueNegativo: 0,
+        recepcionPositivo: 0,
+        recepcionNegativo: 0,
+        ataquePositivo: 0,
+        ataqueNegativo: 0
+      };
+    }
+
+    // Agregar evento de doble clic para cambiar el nombre del jugador
+    celdaNombre.addEventListener('dblclick', function () {
+      const nuevoNombre = prompt('Ingresa el nuevo nombre del jugador:', nombreJugador);
+      if (nuevoNombre !== null && nuevoNombre.trim() !== '') {
+        const nombreAnterior = nombreJugador;
+        const nuevoNombreJugador = nuevoNombre.trim().toUpperCase();
+        estadisticasJugadores[nuevoNombreJugador] = estadisticasJugadores[nombreAnterior];
+        delete estadisticasJugadores[nombreAnterior];
+
+        // Actualizar el contenido de la celda con el nuevo nombre
+        this.textContent = nuevoNombreJugador;
+        // Actualizar la lista de resultados
+        actualizarResultados();
+      }
+    });
   }
 
   // Obtener los botones de flecha de cada columna para el jugador actual
@@ -80,8 +103,24 @@ filasJugadores.forEach(function (fila) {
 
   // Función para incrementar el contador y actualizar el resultado
   function incrementarConteo(contenedor, tipo) {
-    const estadisticas = estadisticasJugadores[nombreJugador];
+
+    const nombreJugador = contenedor.closest('tr').querySelector('.nombre-jugador').textContent;
+    let estadisticas = estadisticasJugadores[nombreJugador];
+    if (!estadisticas) {
+      estadisticasJugadores[nombreJugador] = {
+        saquePositivo: 0,
+        saqueNegativo: 0,
+        recepcionPositivo: 0,
+        recepcionNegativo: 0,
+        ataquePositivo: 0,
+        ataqueNegativo: 0
+      };
+      estadisticas = estadisticasJugadores[nombreJugador];
+    }
     reproducirIncremento();
+
+
+
     if (tipo === 'saque') {
       if (contenedor.firstChild.alt === 'Up') {
         estadisticas.saquePositivo++;
