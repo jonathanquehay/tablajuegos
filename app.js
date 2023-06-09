c7f73f613850bc544f67dc9ed854294be5782211
jonathanquehay <jjmore83@gmail.com>
2023-06-09 17:51:40 -0600

    ultima actualizacion

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 // Obtener todas las filas de la tabla (excepto la primera, que contiene los encabezados)
 const filasJugadores = document.querySelectorAll('tbody tr');
 
+
 // Crear un objeto para almacenar los contadores y porcentajes de cada jugador
 const estadisticasJugadores = {};
 // Iterar sobre cada fila de jugadores
@@ -85,7 +86,7 @@ filasJugadores.forEach(function (fila) {
         // Actualizar el contenido de la celda con el nuevo nombre
         this.textContent = nuevoNombreJugador;
         // Actualizar la lista de resultados
-        actualizarResultados();
+        //actualizarResultados();
       }
     });
   }
@@ -151,22 +152,170 @@ filasJugadores.forEach(function (fila) {
     actualizarResultados();
   }
 
-  // Asignar el evento de clic a los botones de flecha de cada columna
-  botonesSaque.forEach(function (boton) {
-    boton.addEventListener('click', function () {
-      incrementarConteo(this, 'saque');
-    });
+  // Agregar botón Eliminar a la fila
+  const eliminarJugadorBtn = document.createElement('button');
+  eliminarJugadorBtn.textContent = 'Eliminar';
+  eliminarJugadorBtn.style.backgroundColor = 'red';
+  eliminarJugadorBtn.style.color = 'white';
+  eliminarJugadorBtn.style.border = 'none';
+  eliminarJugadorBtn.style.marginTop = '40px';
+  eliminarJugadorBtn.style.padding = '10px 10px';
+  eliminarJugadorBtn.style.borderRadius = '10px';
+  eliminarJugadorBtn.style.cursor = 'pointer';
+  eliminarJugadorBtn.textContent = 'Eliminar';
+  eliminarJugadorBtn.addEventListener('click', function () {
+    // Obtener el nombre del jugador de la celda
+    const nombreJugador = this.closest('tr').querySelector('.nombre-jugador').textContent;
+
+    // Eliminar las estadísticas del jugador
+    delete estadisticasJugadores[nombreJugador];
+
+    // Eliminar la fila de la tabla
+    this.closest('tr').remove();
+
+    // Actualizar la lista de resultados
+    actualizarResultados();
   });
 
-  botonesRecepcion.forEach(function (boton) {
-    boton.addEventListener('click', function () {
-      incrementarConteo(this, 'recepcion');
-    });
-  });
+  // Agregar el botón al final de la fila
+  fila.appendChild(eliminarJugadorBtn);
 
-  botonesAtaque.forEach(function (boton) {
-    boton.addEventListener('click', function () {
-      incrementarConteo(this, 'ataque');
-    });
-  });
+
+  //************** */
+  // Obtener el elemento de la tabla
+  const tablaJugadores = document.querySelector('table');
+
+  // Crear el botón para agregar jugadores
+  const agregarJugadorBtn = document.createElement('button');
+  agregarJugadorBtn.textContent = 'Agregar Jugador';
+  agregarJugadorBtn.style.backgroundColor = 'green';
+  agregarJugadorBtn.style.color = 'white';
+  agregarJugadorBtn.style.marginLeft = '50%';
+  agregarJugadorBtn.style.border = 'none';
+  agregarJugadorBtn.style.marginBottom = '20px';
+  agregarJugadorBtn.style.padding = '10px 10px';
+  agregarJugadorBtn.style.borderRadius = '10px';
+  agregarJugadorBtn.style.cursor = 'pointer';
+
+  // Función para agregar un jugador a la tabla
+  function agregarJugador() {
+    const nombreJugador = prompt('Ingresa el nombre del jugador:');
+    if (nombreJugador && nombreJugador.trim() !== '') {
+      const nuevaFila = document.createElement('tr');
+      const celdaNombre = document.createElement('td');
+      const celdaSaque = document.createElement('td');
+      const celdaRecepcion = document.createElement('td');
+      const celdaAtaque = document.createElement('td');
+
+      const nuevoNombreJugador = nombreJugador.trim().toUpperCase();
+
+      celdaNombre.innerHTML = nuevoNombreJugador;
+      celdaNombre.classList.add('nombre-jugador');
+
+      const botonesSaque = document.createElement('td');
+      const botonSaqueUp = document.createElement('button');
+      const botonSaqueDown = document.createElement('button');
+      const imagenSaqueUp = document.createElement('img');
+      const imagenSaqueDown = document.createElement('img');
+
+      const botonesRecepcion = document.createElement('td');
+      const botonRecepcionUp = document.createElement('button');
+      const botonRecepcionDown = document.createElement('button');
+      const imagenRecepcionUp = document.createElement('img');
+      const imagenRecepcionDown = document.createElement('img');
+
+      const botonesAtaque = document.createElement('td');
+      const botonAtaqueUp = document.createElement('button');
+      const botonAtaqueDown = document.createElement('button');
+      const imagenAtaqueUp = document.createElement('img');
+      const imagenAtaqueDown = document.createElement('img');
+
+      botonSaqueUp.appendChild(imagenSaqueUp);
+      botonSaqueDown.appendChild(imagenSaqueDown);
+      botonRecepcionUp.appendChild(imagenRecepcionUp);
+      botonRecepcionDown.appendChild(imagenRecepcionDown);
+      botonAtaqueUp.appendChild(imagenAtaqueUp);
+      botonAtaqueDown.appendChild(imagenAtaqueDown);
+
+      imagenSaqueUp.src = 'imagenes/arrow_up.png';
+      imagenSaqueUp.alt = 'Up';
+      imagenSaqueDown.src = 'imagenes/arrow_down.png';
+      imagenSaqueDown.alt = 'Down';
+      imagenRecepcionUp.src = 'imagenes/arrow_up.png';
+      imagenRecepcionUp.alt = 'Up';
+      imagenRecepcionDown.src = 'imagenes/arrow_down.png';
+      imagenRecepcionDown.alt = 'Down';
+      imagenAtaqueUp.src = 'imagenes/arrow_up.png';
+      imagenAtaqueUp.alt = 'Up';
+      imagenAtaqueDown.src = 'imagenes/arrow_down.png';
+      imagenAtaqueDown.alt = 'Down';
+
+      botonesSaque.appendChild(botonSaqueUp);
+      botonesSaque.appendChild(botonSaqueDown);
+      botonesRecepcion.appendChild(botonRecepcionUp);
+      botonesRecepcion.appendChild(botonRecepcionDown);
+      botonesAtaque.appendChild(botonAtaqueUp);
+      botonesAtaque.appendChild(botonAtaqueDown);
+
+      nuevaFila.appendChild(celdaNombre);
+      nuevaFila.appendChild(celdaSaque);
+      nuevaFila.appendChild(celdaRecepcion);
+      nuevaFila.appendChild(celdaAtaque);
+
+      celdaSaque.appendChild(botonesSaque);
+      celdaRecepcion.appendChild(botonesRecepcion);
+      celdaAtaque.appendChild(botonesAtaque);
+
+      // Agregar eventos de clic a los botones de flecha para incrementar el conteo
+      botonSaqueUp.addEventListener('click', function () {
+        incrementarConteo(this, 'saque');
+      });
+
+      botonSaqueDown.addEventListener('click', function () {
+        incrementarConteo(this, 'saque');
+      });
+
+      botonRecepcionUp.addEventListener('click', function () {
+        incrementarConteo(this, 'recepcion');
+      });
+
+      botonRecepcionDown.addEventListener('click', function () {
+        incrementarConteo(this, 'recepcion');
+      });
+
+      botonAtaqueUp.addEventListener('click', function () {
+        incrementarConteo(this, 'ataque');
+      });
+
+      botonAtaqueDown.addEventListener('click', function () {
+        incrementarConteo(this, 'ataque');
+      });
+
+      // Agregar la fila al inicio de la tabla
+      tablaJugadores.prepend(nuevaFila);
+
+      // Actualizar la lista de resultados
+      //actualizarResultados();
+
+
+      actualizarResultados();
+
+
+      // Agregar el botón al final de la fila
+      fila.appendChild(eliminarJugadorBtn);
+
+    }
+  }
+
+  // Agregar el evento de clic al botón de agregar jugador
+  agregarJugadorBtn.addEventListener('click', agregarJugador);
+
+  // Agregar el botón al inicio del documento
+  document.body.insertBefore(agregarJugadorBtn, document.body.firstChild);
+
+
+
+  //***************** */
 });
+
+
